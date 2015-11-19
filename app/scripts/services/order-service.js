de061c5448318fb547106915c559d700254a717e
Enouk <tandy.nilsson@gmail.com>
2015-11-19 21:55:54 +0100

    removed tmp path

--- app/scripts/services/order-service.js
+++ app/scripts/services/order-service.js
@@ -5,11 +5,11 @@ angular.module('washerApp')
   .service('OrderService', function($http) {
 
     this.getCurrent = function() {
-      return $http.get('washer/data/orders.json');
+      return $http.get('data/orders.json');
     };
 
     this.getHistory = function() {
-      return $http.get('washer/data/history.json');
+      return $http.get('data/history.json');
     };
 
   });
